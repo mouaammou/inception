192dee03d93b1102ed89e6df3f3214d4961ca8d8
mouaammou <mouadamassine@gmail.com>
2024-07-13 14:24:17 +0100

    add .env to gitignore

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 service mariadb start 
 
-echo "CREATE DATABASE IF NOT EXISTS $database ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$user'@'%' IDENTIFIED BY '$password' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $database.* TO '$user'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$root_password' ;" >> db1.sql
+echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE ;" > db1.sql
+echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD' ;" >> db1.sql
+echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' ;" >> db1.sql
+echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASS' ;" >> db1.sql
 echo "FLUSH PRIVILEGES;" >> db1.sql
 
 mysql < db1.sql
