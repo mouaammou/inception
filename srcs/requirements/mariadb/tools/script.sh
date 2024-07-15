83c8c2964da800b2379b863c106ef80b0af1cc29
mouaammou <mouadamassine@gmail.com>
2024-07-15 13:35:16 +0100

    mime types

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 service mariadb start 
 
-mysql -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE ;"
-mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD' ;" 
-mysql -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' ;" 
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASS' ;" 
-mysql -e "FLUSH PRIVILEGES;" 
+mysql -u root -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE ;"
+mysql -u root -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD' ;" 
+mysql -u root -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' ;" 
+mysql -u root -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASS' ;" 
+mysql -u root -e "FLUSH PRIVILEGES;" 
 
 
 kill $(cat /var/run/mysqld/mysqld.pid)
