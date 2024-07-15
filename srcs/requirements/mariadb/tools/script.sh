a05a52b457d3e1b5612047a64b7ea03ec45222a1
mouaammou <mouadamassine@gmail.com>
2024-07-15 10:41:43 +0100

    autop

--- srcs/requirements/mariadb/tools/script.sh
+++ srcs/requirements/mariadb/tools/script.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 service mariadb start 
 
-echo "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE ;" > db1.sql
-echo "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD' ;" >> db1.sql
-echo "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' ;" >> db1.sql
-echo "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASS' ;" >> db1.sql
-echo "FLUSH PRIVILEGES;" >> db1.sql
+mysql -e "CREATE DATABASE IF NOT EXISTS $SQL_DATABASE ;"
+mysql -e "CREATE USER IF NOT EXISTS '$SQL_USER'@'%' IDENTIFIED BY '$SQL_PASSWORD' ;" 
+mysql -e "GRANT ALL PRIVILEGES ON $SQL_DATABASE.* TO '$SQL_USER'@'%' ;" 
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$ROOT_PASS' ;" 
+mysql -e "FLUSH PRIVILEGES;" 
 
-mysql < db1.sql
 
 kill $(cat /var/run/mysqld/mysqld.pid)
 
